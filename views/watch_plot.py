33032699cf4482001648942f6831e86e208572e3
Balázs Vörös <bvoros@ppfeurope.com>
2025-04-25 23:49:11 +0200

    yooooooooooooo

--- views/watch_plot.py
+++ views/watch_plot.py
@@ -6,12 +6,61 @@ from utils import get_data
 # Loading data
 df = get_data()
 
+# Calculating dates
+latest_update = df['date'].max()
+second_latest_update = df['date'][df['date'] < latest_update].max()
+
+# Calculating number of ads
+num_latest = df[df['date'] == latest_update].shape[0]
+num_second_latest = df[df['date'] == second_latest_update].shape[0]
+
+# Calculate the nominal difference and percentage difference
+nominal_diff = num_latest - num_second_latest
+percentage_diff = (nominal_diff / num_second_latest * 100) if num_second_latest != 0 else 0
+
 # Page title
 st.title("Metrics Overview")
 
+# Description of the page
 st.markdown("## Visualize your data insights through interactive charts")
 st.markdown("Explore trends, correlations, and patterns with dynamic graphs to make data analysis easier and more intuitive.")
+st.markdown("---") 
+
+# Format the results in markdown with HTML and CSS
+st.markdown(
+    f"""
+    <div style="background-color: #778da9; color: #ffffff; padding: 15; border-radius: 10px; text-align: center;">
+        <h3>🔄 Latest Update: {latest_update}</h3>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
+
+# Create a grid with 3 smaller tiles below the latest update
+st.markdown(
+    f"""
+    <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+        <div style="background-color: #1b263b; color: #ffffff; padding: 10; border-radius: 10px; width: 30%; text-align: center;">
+            <h4>{num_latest}</h4>
+            <p>Ads in the latest dataset</p>
+        </div>
+        <div style="background-color: #1b263b; color: #ffffff; padding: 10; border-radius: 10px; width: 30%; text-align: center;">
+            <h4>{nominal_diff}</h4>
+            <p>Ads since the previous update</p>
+        </div>
+        <div style="background-color: #1b263b; color: #ffffff; padding: 10; border-radius: 10px; width: 30%; text-align: center;">
+            <h4>{percentage_diff:.2f}%</h4>
+            <p>Market growth (in number of ads)</p>
+        </div>
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
+
 st.markdown("---")
+
+
+
 
 # Creating a copy of the DataFrame for filtering
 filtered_df = df.copy()
